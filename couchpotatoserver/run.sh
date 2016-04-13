db6f6159f0f9b4ccc6cc71086b5df48d8c63936e
Sebastian Juarez <ssebbass@gmail.com>
2016-04-13 16:12:29 -0300

    Added dockerfile user feature

--- couchpotatoserver/run.sh
+++ couchpotatoserver/run.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 set -x
 
-cd /opt/CouchPotatoServer
-git pull
-exec su - couchpotato -c "python CouchPotato.py"
-
+cd /opt/CouchPotatoServer && git pull
+exec /usr/bin/python /opt/CouchPotatoServer/CouchPotato.py
