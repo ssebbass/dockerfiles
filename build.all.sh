f515e07daa9ec0ff71d0c773d47d081967b00b60
Sebastian Juarez <ssebbass@gmail.com>
2016-05-24 13:10:39 -0300

    Updated script for multibuild

--- build.all.sh
+++ build.all.sh
@@ -1,8 +1,32 @@
 #!/bin/bash
-set -x
 set -e
+REPO="ssebbass"
 
-for DIR in */ ; do 
-  IMG="$(basename $DIR)"
-  docker build -t ssebbass/$IMG ./$IMG/
-done
+function build {
+    set -x
+    docker build -t $REPO/$1 ./$1
+    docker push $REPO/$1
+    set +x
+}
+
+function multi {
+  echo "Running multi build..."
+  for DIR in */ ; do 
+    build $(basename $DIR)
+  done
+}
+
+function single {
+  echo "Running \"$1\" build..."
+  build $1
+}
+
+function main {
+  if [ "$1" != "" ]; then
+    single $1
+  else
+    multi
+  fi
+}
+
+main $1
