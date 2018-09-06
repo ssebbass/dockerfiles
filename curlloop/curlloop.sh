19b11929e3ffe54d8d7c6cf24ca5f209ccaba3ff
Sebastian Juarez <ssebbass@gmail.com>
2018-09-06 16:33:18 -0300

    Updated

--- curlloop/curlloop.sh
+++ curlloop/curlloop.sh
@@ -1,13 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 set -x
 
 if [ ! -z "${URL}" ]; then
   while true; do
-    curl "${URL}" >/dev/null
-    sleep "${SLEEP}"
+    eval "curl {$URL} >/dev/null"
+    sleep .$[ ( $RANDOM % 10 ) + 1 ]s
   done
 else
   echo "Please set URL env variable"
   exit 1
 fi
-
